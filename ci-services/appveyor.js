d19471178a0d294fe8c0aa10771061c7bfafb47a
Simon Chan <cnsimonchan@live.com>
2017-09-21 11:38:35 +0800

    Add support for AppVeyor

---
+++ ci-services/appveyor.js
@@ -0,0 +1,11 @@
+const gitHelpers = require('../lib/git-helpers')
+
+const env = process.env
+
+module.exports = {
+  repoSlug: env.APPVEYOR_REPO_NAME,
+  branchName: env.APPVEYOR_REPO_BRANCH,
+  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.APPVEYOR_REPO_BRANCH) === 1,
+  correctBuild: env.APPVEYOR_PULL_REQUEST_NUMBER === '',
+  uploadBuild: env.APPVEYOR_JOB_NUMBER === '1'
+}
