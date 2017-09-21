dc3d463d84239352e1f8a7320f2dd1b609db0f70
Simon Chan <cnsimonchan@live.com>
2017-09-21 11:55:30 +0800

    Use `bash` on AppVeyor

--- ci-services/appveyor.js
+++ ci-services/appveyor.js
@@ -1,13 +1,26 @@
-const gitHelpers = require('../lib/git-helpers')
+const exec = require('child_process').execSync
+
+const _ = require('lodash')
 
 const env = process.env
 
-console.log(JSON.stringify(env));
+// AppVeyor runs on Windows machines.
+// Use `bash` to invoke the command.
+function getNumberOfCommitsOnBranch(branch) {
+  const refArgument = `$(git for-each-ref '--format=%(refname)' refs/ | grep /${branch} | head -1)`
+  const notArgument = `$(git for-each-ref '--format=%(refname)' refs/ | grep -v /${branch})`
+  const gitCommand = `git log ${refArgument} --oneline --not ${notArgument} | wc -l`
+  const result = exec(
+    `C:/cygwin/bin/bash -lc "${gitCommand}"`
+  ).toString()
+  console.log(result);
+  return _.toNumber(result)
+}
 
 module.exports = {
   repoSlug: env.APPVEYOR_REPO_NAME,
   branchName: env.APPVEYOR_REPO_BRANCH,
-  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.APPVEYOR_REPO_BRANCH) === 1,
+  firstPush: getNumberOfCommitsOnBranch(env.APPVEYOR_REPO_BRANCH) === 1,
   correctBuild: env.APPVEYOR_PULL_REQUEST_NUMBER === '',
   uploadBuild: env.APPVEYOR_JOB_NUMBER === '1'
 }
